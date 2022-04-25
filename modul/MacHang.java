12b13b7c521aa9268b780594ebefc47beed70a49
nasty091 <6151071035@st.utc2.edu.vn>
2022-04-25 16:32:50 +0700

    Update MacHang.java

--- modul/MacHang.java
+++ modul/MacHang.java
@@ -1,33 +1,61 @@
-package modul;
+package model;
 
-import java.util.Date;
+import java.sql.Date;
 import java.util.Objects;
 
 public class MacHang {
 	private int maMacHang;
 	private String tenMacHang;
-	private Tinh xuatXu;
 	private Date ngayNhap;
 	private Date ngayHetHang;
 	private float giaNhap;
 	private float giaBan;
 	private float canNang;
+	private DaiLy xuatXu;
 	
 	public MacHang() {
 		
 	}
 
-	public MacHang(int maMacHang, String tenMacHang, Tinh xuatXu, Date ngayNhap, Date ngayHetHang, float giaNhap,
-			float giaBan, float canNang) {
+	public MacHang(int maMacHang, String tenMacHang, Date ngayNhap, Date ngayHetHang, float giaNhap, float giaBan,
+			float canNang, DaiLy xuatXu) {
 		super();
 		this.maMacHang = maMacHang;
 		this.tenMacHang = tenMacHang;
-		this.xuatXu = xuatXu;
 		this.ngayNhap = ngayNhap;
 		this.ngayHetHang = ngayHetHang;
 		this.giaNhap = giaNhap;
 		this.giaBan = giaBan;
 		this.canNang = canNang;
+		this.xuatXu = xuatXu;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(canNang, giaBan, giaNhap, maMacHang, ngayHetHang, ngayNhap, tenMacHang, xuatXu);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MacHang other = (MacHang) obj;
+		return Float.floatToIntBits(canNang) == Float.floatToIntBits(other.canNang)
+				&& Float.floatToIntBits(giaBan) == Float.floatToIntBits(other.giaBan)
+				&& Float.floatToIntBits(giaNhap) == Float.floatToIntBits(other.giaNhap) && maMacHang == other.maMacHang
+				&& Objects.equals(ngayHetHang, other.ngayHetHang) && Objects.equals(ngayNhap, other.ngayNhap)
+				&& Objects.equals(tenMacHang, other.tenMacHang) && Objects.equals(xuatXu, other.xuatXu);
+	}
+
+	@Override
+	public String toString() {
+		return "MacHang [maMacHang=" + maMacHang + ", tenMacHang=" + tenMacHang + ", ngayNhap=" + ngayNhap
+				+ ", ngayHetHang=" + ngayHetHang + ", giaNhap=" + giaNhap + ", giaBan=" + giaBan + ", canNang="
+				+ canNang + ", xuatXu=" + xuatXu + "]";
 	}
 
 	public int getMaMacHang() {
@@ -44,14 +72,6 @@ public class MacHang {
 
 	public void setTenMacHang(String tenMacHang) {
 		this.tenMacHang = tenMacHang;
-	}
-
-	public Tinh getXuatXu() {
-		return xuatXu;
-	}
-
-	public void setXuatXu(Tinh xuatXu) {
-		this.xuatXu = xuatXu;
 	}
 
 	public Date getNgayNhap() {
@@ -94,36 +114,11 @@ public class MacHang {
 		this.canNang = canNang;
 	}
 
-	@Override
-	public String toString() {
-		return "MacHang [maMacHang=" + maMacHang + ", tenMacHang=" + tenMacHang + ", xuatXu=" + xuatXu + ", ngayNhap="
-				+ ngayNhap + ", ngayHetHang=" + ngayHetHang + ", giaNhap=" + giaNhap + ", giaBan=" + giaBan
-				+ ", canNang=" + canNang + "]";
+	public DaiLy getXuatXu() {
+		return xuatXu;
 	}
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(canNang, giaBan, giaNhap, maMacHang, ngayHetHang, ngayNhap, tenMacHang, xuatXu);
+	public void setXuatXu(DaiLy xuatXu) {
+		this.xuatXu = xuatXu;
 	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		MacHang other = (MacHang) obj;
-		return Float.floatToIntBits(canNang) == Float.floatToIntBits(other.canNang)
-				&& Float.floatToIntBits(giaBan) == Float.floatToIntBits(other.giaBan)
-				&& Float.floatToIntBits(giaNhap) == Float.floatToIntBits(other.giaNhap) && maMacHang == other.maMacHang
-				&& Objects.equals(ngayHetHang, other.ngayHetHang) && Objects.equals(ngayNhap, other.ngayNhap)
-				&& Objects.equals(tenMacHang, other.tenMacHang) && Objects.equals(xuatXu, other.xuatXu);
-	}
-	
-	
-	
-	
-	
 }
