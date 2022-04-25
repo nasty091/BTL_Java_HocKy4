090ad45e7f426e90ae69dfe83efa1cfd6f56db9f
nasty091 <6151071035@st.utc2.edu.vn>
2022-04-25 16:32:52 +0700

    Update QLCHModel.java

--- modul/QLCHModel.java
+++ modul/QLCHModel.java
@@ -1,39 +1,35 @@
-package modul;
+package model;
 
 import java.util.ArrayList;
 
 public class QLCHModel {
 	private ArrayList<MacHang> dsMacHang;
 
-	public QLCHModel() {
-		this.dsMacHang = new ArrayList<MacHang>();
-	}
-
 	public QLCHModel(ArrayList<MacHang> dsMacHang) {
+		super();
 		this.dsMacHang = dsMacHang;
 	}
-	
+
 	public ArrayList<MacHang> getDsMacHang() {
 		return dsMacHang;
 	}
-	
+
 	public void setDsMacHang(ArrayList<MacHang> dsMacHang) {
 		this.dsMacHang = dsMacHang;
 	}
-	
+	//Hàm thêm hàng
 	public void insert(MacHang macHang) {
 		this.dsMacHang.add(macHang);
 	}
-	
+	//Hàm xóa hàng
 	public void delete(MacHang macHang) {
 		this.dsMacHang.remove(macHang);
 	}
-	
+	//Hàm cập nhập hàng
 	public void update(MacHang macHang) {
 		this.dsMacHang.remove(macHang);
 		this.dsMacHang.add(macHang);
 	}
-	
 	
 }
 
