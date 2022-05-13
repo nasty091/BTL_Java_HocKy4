3eec0c9afcaac37c81c0d5567eefb01e3d319bfe
nasty091 <6151071035@st.utc2.edu.vn>
2022-05-13 10:50:21 +0700

    Update QLCHView.java

--- view/QLCHView.java
+++ view/QLCHView.java
@@ -1,6 +1,7 @@
 package view;
 
 import java.awt.BorderLayout;
+<<<<<<< HEAD
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
@@ -42,6 +43,8 @@ import javax.swing.JScrollPane;
 import javax.swing.table.DefaultTableModel;
 
 import controller.QLCHController;
+=======
+>>>>>>> 03c331c258b0e5dee213c76151e2c81dc651a403
 
 public class QLCHView extends JFrame {
 
@@ -287,27 +290,43 @@ public class QLCHView extends JFrame {
 		//Dùng các nút nhấn
 		btnXoa.addActionListener(action);
 		btnXoa.setFont(new Font("Tahoma", Font.PLAIN, 20));
+<<<<<<< HEAD
 		btnXoa.setBounds(61, 689, 133, 37);
+=======
+		btnXoa.setBounds(68, 689, 133, 37);
+>>>>>>> 03c331c258b0e5dee213c76151e2c81dc651a403
 		contentPane.add(btnXoa);
 		
 		JButton btnCapNhat = new JButton("C\u1EADp nh\u1EADt");
 		//Dùng các nút nhấn
 		btnCapNhat.addActionListener(action);
 		btnCapNhat.setFont(new Font("Tahoma", Font.PLAIN, 20));
+<<<<<<< HEAD
 		btnCapNhat.setBounds(282, 689, 133, 37);
+=======
+		btnCapNhat.setBounds(245, 689, 133, 37);
+>>>>>>> 03c331c258b0e5dee213c76151e2c81dc651a403
 		contentPane.add(btnCapNhat);
 		
 		JButton btnLuu = new JButton("L\u01B0u");
 		btnLuu.addActionListener(action);
 		btnLuu.setFont(new Font("Tahoma", Font.PLAIN, 20));
+<<<<<<< HEAD
 		btnLuu.setBounds(506, 689, 133, 37);
+=======
+		btnLuu.setBounds(420, 689, 133, 37);
+>>>>>>> 03c331c258b0e5dee213c76151e2c81dc651a403
 		contentPane.add(btnLuu);
 		
 		JButton btnHuyBo = new JButton("H\u1EE7y b\u1ECF");
 		//Dùng các nút nhấn
 		btnHuyBo.addActionListener(action);
 		btnHuyBo.setFont(new Font("Tahoma", Font.PLAIN, 20));
+<<<<<<< HEAD
 		btnHuyBo.setBounds(731, 689, 133, 37);
+=======
+		btnHuyBo.setBounds(601, 689, 133, 37);
+>>>>>>> 03c331c258b0e5dee213c76151e2c81dc651a403
 		contentPane.add(btnHuyBo);
 		
 		JSeparator separator_2 = new JSeparator();
@@ -319,7 +338,11 @@ public class QLCHView extends JFrame {
 		btnHuyTim.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		btnHuyTim.setBounds(782, 29, 115, 47);
 		contentPane.add(btnHuyTim);
+<<<<<<< HEAD
 		
+=======
+
+>>>>>>> 03c331c258b0e5dee213c76151e2c81dc651a403
 		this.setVisible(true);
 	}
 
@@ -334,7 +357,11 @@ public class QLCHView extends JFrame {
 		comboBox_DaiLy.setSelectedIndex(-1);
 	}
 	
+<<<<<<< HEAD
 	//Hàm đẩy các thông tin đã nhập lên bảng
+=======
+    //Hàm đẩy các thông tin đã nhập lên bảng
+>>>>>>> 03c331c258b0e5dee213c76151e2c81dc651a403
 	public void themMacHangVaoTable(MacHang mh) {
 		DefaultTableModel model_table = (DefaultTableModel) table.getModel();
 		model_table.addRow(
@@ -462,7 +489,11 @@ public class QLCHView extends JFrame {
 	//Hàm Tìm kiếm
 	public void thucHienTim() {
 		//Hàm thực hiện hủy tìm, nếu không có sẽ bị lỗi dữ liệu 
+<<<<<<< HEAD
 		this.thucHienTaiLaiDuLieu();
+=======
+		this.thucHienHuyTim();
+>>>>>>> 03c331c258b0e5dee213c76151e2c81dc651a403
 		
 		//Hàm thực hiện tìm
 		int xuatXu = this.comboBox_DaiLy_TimKiem.getSelectedIndex()-1;
@@ -508,7 +539,11 @@ public class QLCHView extends JFrame {
 	}
 	
 	//Hàm hủy tìm và tải lại dữu liệu đã nhập lên bảng
+<<<<<<< HEAD
 	public void thucHienTaiLaiDuLieu() {
+=======
+	public void thucHienHuyTim() {
+>>>>>>> 03c331c258b0e5dee213c76151e2c81dc651a403
 		//Xóa cái tìm trong bảng đi
 		while (true) {
 			DefaultTableModel model_table = (DefaultTableModel) table.getModel();
@@ -527,6 +562,7 @@ public class QLCHView extends JFrame {
 			this.themMacHangVaoTable(mh);
 		}
 	}
+<<<<<<< HEAD
 
 	public void hienThiAbout() {
 		JOptionPane.showMessageDialog(this, "Phần mềm quản lý mặc hàng 2.0!");
@@ -599,4 +635,6 @@ public class QLCHView extends JFrame {
 	}
 
 
+=======
+>>>>>>> 03c331c258b0e5dee213c76151e2c81dc651a403
 }
