aad73534d67812630985831ee1cb72e9937db61b
Bao2606 <79888313+Bao2606@users.noreply.github.com>
2022-05-18 11:23:50 +0700

    Update DaiLy.java

--- model/DaiLy.java
+++ model/DaiLy.java
@@ -1,8 +1,9 @@
 package model;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class DaiLy {
+public class DaiLy implements Serializable {
 	private int maDaiLy;
 	private String tenDaiLy;
 	
